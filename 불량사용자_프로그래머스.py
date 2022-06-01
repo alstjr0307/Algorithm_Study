61285497cbf5fdd5d27eea56d23e8ca3a9f3876f
alstjr0307 <alswp07@naver.com>
2022-06-02 01:45:26 +0900

    불량사용자

--- 불량사용자_프로그래머스.py
+++ 불량사용자_프로그래머스.py
@@ -1,20 +1,27 @@
-import sys
-sys.setrecursionlimit(100000)
+from itertools import permutations
+def check(a,b): #a와 b가 같은 유저인지 판별하는 함수
+    cnt=0
+    if len(a)!=len(b):
+        return False
+    for i in range(len(a)):
+        if a[i] == b[i] or b[i] =='*':
+            cnt+=1
+    if cnt == len(a):
+        return True
+    else: return False
 
 def solution(user_id, banned_id):
-    answer = 0
-    len_ban = len(banned_id)
-    visit = [False for _ in range(len_ban)]
-    for i in range(len_ban):
-        
-def dfs(ban,ind,user):
-    for i in user:
-        if len(ban[ind]) == len(i):
-            for j in range(len(i)):
-                if i[j] == ban[ind][j] or ban[ind][j]=='*':
-                    tmp = user.copy()
-                    tmp.remove(i)
-                    if i<len(ban):
-                        dfs(ban,ind+1,tmp)
+    answer=[]
+    for i in permutations(user_id, len(banned_id)): #permutaion 함수는 a리스트에서 b개를 포함하는 조합을 만들어줌
 
+        count=0
+        for a,b in zip(i,banned_id):
+
+            if check(a,b):
+                count+=1
+        if count == len(banned_id):
+            if set(i) not in answer: #아이디 순서에 상관 없다했으므로 set 사용
+                answer.append(set(i))
+ 
+    return len(answer)
 solution(["frodo", "fradi", "crodo", "abc123", "frodoc"],["fr*d*", "abc1**"])
