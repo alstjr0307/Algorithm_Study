e02e2fe79966cdf12802ed41321de5ecd1410ec7
alstjr0307 <alswp07@naver.com>
2023-09-11 17:33:20 +0900

    2023.9.11

--- 2225합분해_백준.py
+++ 2225합분해_백준.py
@@ -1,11 +1,5 @@
-n,k = map(int,input().split())
-all=[]
-dp = [1 for _ in range(1,n+1)]
-all.append(dp)
-if n ==1:
-    print(k)
-
-else:
-    for i in range(k-1):
-        ims = [0 for _ in range(1,n+1)]
-        for j in 
+n, k = map(int,input().split())
+dp = [0 for _ in range(n+1)]
+dp[0]=1
+dp[1]=k
+dp[2] = 
