bb6e9e484c05e4ea4033983f167c72a95c2ba8ed
alstjr0307 <alswp07@naver.com>
2023-09-15 01:30:24 +0900

    ㅁ

--- 2225합분해_백준.py
+++ 2225합분해_백준.py
@@ -1,5 +1,9 @@
-n, k = map(int,input().split())
-dp = [0 for _ in range(n+1)]
-dp[0]=1
-dp[1]=k
-dp[2] = 
+k, n = map(int,input().split())
+data=[1 for _ in range(k+1)]
+dp=[[1 for i in range(k+1)] for _ in range(n+1)]
+
+for i in range(2, n+1):
+    for j in range(k+1):
+
+        dp[i][j] = sum(dp[i-1][0:j+1])
+print(dp[n][k]%1000000000)
